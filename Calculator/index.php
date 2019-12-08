f8a385086d18f1635091186915009a4b885763d7
Campeanu <campeanu.it@yahoo.com>
2019-12-08 23:53:26 +0200

    Revision 2

--- Calculator/index.php
+++ Calculator/index.php
@@ -6,10 +6,26 @@
 		<meta name="viewport" content="width=device-width, initial-scale=1">
 		<link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css" rel="stylesheet">
 	</head>
+	<style>
+		body { 
+			background: url(http://imgint.112.agency/original/2019/07/11/284545.jpg) no-repeat center center fixed; 
+			-webkit-background-size: cover;
+			-moz-background-size: cover;
+			-o-background-size: cover;
+			background-size: cover;
+		}
+		.container {
+			background: rgba(255, 255, 255, 0.7)    /*  10% opaque green */ ;
+			padding:10px;
+		}
+	</style>
 	<body>
 		<center>
-			<div class="container" style="margin-top: 220px">
-				<h1>Campeanu Vasile - Project 2</h1>
+			<div class="container" style="margin-top: 220px;">
+				<h1> <b>Campeanu Vasile</b> - Project 2</h1>
+				<h2>Rezistenta: <span style="color: red; font-weight:bold;">R = u / i</span><h2>
+				<h2>Tensiunea: <span style="color: red; font-weight:bold;">U = i * r</span></h2>
+				<h2>Intensitatea: <span style="color: red; font-weight:bold;">I = U / r</span></h2>
 				<?php
 					if(isset($_POST['submit']))
 					{
@@ -18,32 +34,47 @@
 							if($_POST['operation'] == 'plus')
 							{
 								$total = $_POST['number1'] + $_POST['number2'];	
+								// header("Location: index.php");
 							}
 							if($_POST['operation'] == 'minus')
 							{
 								$total = $_POST['number1'] - $_POST['number2'];	
+								// header("Location: index.php");
 							}
 							if($_POST['operation'] == 'multiply')
 							{
 								$total = $_POST['number1'] * $_POST['number2'];	
+								// header("Location: index.php");
 							}
 							if($_POST['operation'] == 'dividedby')
 							{
 								$total = $_POST['number1'] / $_POST['number2'];	
+								// header("Location: index.php");
 							}
-							if($_POST['operation'] == 'operationx')
+							if($_POST['operation'] == 'intensity')
 							{
-								$total = $_POST['number1'] + $_POST['number2'];	
+								$total = $_POST['number1'] / $_POST['number2'];	
+								// header("Location: index.php");
 							}	
-							if($_POST['operation'] == 'operationy')
+							if($_POST['operation'] == 'voltage')
 							{
-								$total = $_POST['number1'] + $_POST['number2'];	
+								$total = $_POST['number1'] * $_POST['number2'];	
+								// header("Location: index.php");
 							}	
-							if($_POST['operation'] == 'operationz')
+							if($_POST['operation'] == 'resistance')
 							{
-								$total = $_POST['number1'] + $_POST['number2'];	
+								$total = $_POST['number1'] / $_POST['number2'];	
+								// header("Location: index.php");
 							}			
-							echo "<h1 style='color:green; margin-top:20px;'>{$_POST['number1']} {$_POST['operation']} {$_POST['number2']} equals {$total}</h1>";
+							
+							if($_POST['operation'] == "intensity")
+								echo "<h1 style='color:green; margin-top:20px;'> Intensitatea este egala cu: {$total} </h1>";
+							else if($_POST['operation'] == "voltage")
+								echo "<h1 style='color:green; margin-top:20px;'> Tensiunea este egala cu: {$total} </h1>";
+							else if($_POST['operation'] == "resistance")
+								echo "<h1 style='color:green; margin-top:20px;'> Rezistnta este egala cu: {$total} </h1>";
+							else
+								echo "<h1 style='color:green; margin-top:20px;'> {$_POST['number1']} {$_POST['operation']} {$_POST['number2']} equals {$total} </h1>";
 						} 
 						else 
 						{
@@ -53,16 +84,16 @@
 				?>
 				<form method="post" action="index.php">
 					<input name="number1" type="text" class="form-control" style="width: 150px; display: inline" />
-					<select name="operation">
+					<input name="number2" type="text" class="form-control" style="width: 150px; display: inline" />
+					<select name="operation" class="form-control" style="width: 150px; display: inline">
+						<option value="intensity">Intensity</option>
+						<option value="voltage">Voltage</option>
+						<option value="resistance">Resistance</option>
 						<option value="plus">Plus</option>
 						<option value="minus">Minus</option>
 						<option value="multiply">Multiply</option>
 						<option value="dividedby">Devide</option>
-						<option value="operationx">OperationX</option>
-						<option value="operationy">OPerationY</option>
-						<option value="operationz">OPerationZ</option>
 					</select>
-					<input name="number2" type="text" class="form-control" style="width: 150px; display: inline" />
 					<input name="submit"  type="submit" value="Calculate" class="btn btn-primary" />
 				</form>
 			</div>
